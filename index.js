a0890600dabd604f40ab36a9415f778bfbae5f4b
czandmf <82135080+czandmf@users.noreply.github.com>
2021-04-08 13:46:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		select1: 1, // 符文石
+		select2: 1, // 品质
+		ap1: "1",
+		va1: "",
+		ap2: "2",
+		va2: "",
+		ap3: "3",
+		va3: "",
+		value4: "3",
+		value5: "3",
+		value6: 2,
+		value7: 5,
+		test: ""
+	},
+	methods: {
+		count: function() {
+			
+		}
+	}
+});
